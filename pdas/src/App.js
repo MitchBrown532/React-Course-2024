ee345124c2508ec4725bbb9f3d629706f74b3528
MitchBrown532 <55161234+MitchBrown532@users.noreply.github.com>
2024-05-15 18:12:46 -0400

    Styling
    Learning Bulma and adding styling to the app

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,13 +1,30 @@
 import ProfileCard from "./ProfileCard";
+import AlexaImage from './images/alexa.png'
+import CortanaImage from './images/cortana.png'
+import SiriImage from './images/siri.png'
+import 'bulma/css/bulma.css'
+
 
 function App() {
     return (
         <div>
             <div>Personal Digital Assistants</div>
 
-            <ProfileCard title = "Alexa" handle = "@alexa99"/>
-            <ProfileCard title = "Cortana" handle = "@Cortana32"/>
-            <ProfileCard title = "Siri" handle = "@Siri01"/>
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-4">
+                            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard title="Cortana" handle="@Cortana32" image={CortanaImage} />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard title="Siri" handle="@Siri01" image={SiriImage} />
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     )
 }
--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -2,18 +2,24 @@
 // Javascript feature - not exclusive to React
 // example: passing { title, handle } rather than passing entire props object
 
-function ProfileCard({ title, handle }){
-    
-    //const title = props.title
-    //const handle = props.handle
-
-    //const { title, handle } = props
-
+function ProfileCard({ title, handle, image }){
 
     return(
-        <div>
-            <div>The title is {title}</div>
-            <div>The handle is {handle}</div>
+        <div className = "card" >
+
+            <div className = "card-iamge" >
+                <figure>
+                    <img src={image} alt="pda logo" />
+                </figure>
+            </div>
+
+            <div className = "card-content">
+                <div className = "media-content">
+                    <p className = "title is-4">{title}</p>
+                    <p className = "subtitle is-6">{handle}</p>
+                </div>
+            </div>
+            
         </div>
     )
 }
