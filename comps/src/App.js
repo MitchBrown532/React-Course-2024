fb28b05a7f82ff5a65fdeb0e9dafa0f453aae5f1
MitchBrown532 <55161234+MitchBrown532@users.noreply.github.com>
2024-05-28 18:05:36 -0400

    New Project: comps
    Practicing creating many different components. This will also involve extensive practice with states, effects, contexts, etc. This will also involve using Tailwind CSS to practice styling.

---
+++ comps/src/App.js
@@ -0,0 +1,28 @@
+import Button from "./Button"
+
+function App() {
+    return (
+        <div>
+            <div>
+                <Button>Click me!</Button>
+            </div>
+            <div>
+                <Button>Buy now</Button>
+            </div>
+            <div>
+                <Button>Blah blah</Button>
+            </div>
+            <div>
+                <Button>sOk </Button>
+            </div>
+            <div>
+                <Button> got it</Button>
+            </div>
+            <div>
+                <Button> one more </Button>
+            </div>
+        </div>
+    )
+}
+
+export default App
