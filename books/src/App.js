1cb35f73db865b57108889c1f18a8e781cf1b4f1
MitchBrown532 <55161234+MitchBrown532@users.noreply.github.com>
2024-05-27 20:21:18 -0400

    Data persistence
    Using Json Server as a mock API to store our data. Then used axios to fetch & edit data when needed.

--- books/src/App.js
+++ books/src/App.js
@@ -1,48 +1,68 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList'
+
 
 
 function App() {
     const [books, setBooks] = useState([])
 
-    const createBook = (title) => {
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books')
+    
+        setBooks(response.data)
+    }
+
+    useEffect(() => {
+        fetchBooks()
+    }, [])
+
+    const createBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title
+        })
+
+        console.log(response)
+
         const updatedBooks = [
             ...books,
-            {
-                id: Math.round(Math.random() * 9999),
-                title,
-            }
+            response.data
         ]
         setBooks(updatedBooks)
     }
 
     const deleteBookById = (id) => {
+        const response = axios.delete(`http://localhost:3001/books/${id}`)
+        
         const updatedBooks = books.filter((book) => {
             return book.id !== id
+        })
+        setBooks(updatedBooks)
+    }
+
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle
+        })
+
+        const updatedBooks = books.map((book) => {
+            if (book.id === id) {
+                return { ...book, ...response.data }
+            }
+            return book
         })
 
         setBooks(updatedBooks)
     }
 
-    const editBookById = (id, newTitle) => {
-        const updatedBooks = books.map((book) => {
-            if (book.id === id) {
-                return {...book, title:newTitle}
-            }
-            return book
-        })
-        
-        setBooks(updatedBooks)
-        } 
 
-        
-    
+
 
     return (
         <div className='app'>
             <h1>Reading List</h1>
-            <BookList books={books} onDelete = {deleteBookById} onEdit = {editBookById}/>
+            <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
             <BookCreate onCreate={createBook} />
         </div>
     )
