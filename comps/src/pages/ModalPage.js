6e1d8a56044b180462ecc90ec932a1842e36be5d
MitchBrown532 <55161234+MitchBrown532@users.noreply.github.com>
2024-05-30 17:07:26 -0400

    Created a Modal for comps
    The focus of creating this modal was getting more practice with proper routing, writing reusable components, using React-Portal, & Tailwind CSS + styling.

---
+++ comps/src/pages/ModalPage.js
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import Modal from '../components/Modal'
+import Button from '../components/Button'
+
+function ModalPage() {
+    const [showModal, setShowModal] = useState(false)
+
+    const handleClick = () => {
+        setShowModal(true)
+    }
+
+    const handleClose = () => {
+        setShowModal(false)
+    }
+
+    const actionBar = <div>
+        <Button primary onClick={handleClose}>I accept</Button>
+    </div>
+
+    const modal = <Modal onClose={handleClose} actionBar={actionBar}>
+        <p>Here is an important piece of information. Please read it very carefully.</p>
+    </Modal>
+
+    return (
+        <div>
+            <Button onClick={handleClick} primary>Open modal</Button>
+            {showModal && modal}
+        </div>
+
+    )
+}
+
+export default ModalPage
