b96dfad57d01d91b747bac2fbc9afe9f18d74e03
MitchBrown532 <55161234+MitchBrown532@users.noreply.github.com>
2024-05-30 21:12:18 -0400

    Practice w/ Hooks
    Created 1 component (Counter) specifically for practice building a hook. Then applied that practice to the SortedTables component I just finished making. This helps me have reusable functionality.

--- comps/src/components/Sidebar.js
+++ comps/src/components/Sidebar.js
@@ -2,9 +2,10 @@ import Link from "./Link";
 
 function Sidebar() {
   const links = [
+    { label: "Buttons", path: "/buttons" },
+    { label: "Counter", path: "/counter" },
     { label: "Dropdown", path: "/" },
     { label: "Accordion", path: "/accordion" },
-    { label: "Buttons", path: "/buttons" },
     { label: "Modal", path: "/modal" },
     { label: "Table", path: "/table" },
   ];
