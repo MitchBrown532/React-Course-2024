7cf2a99ad3b266cb6c250c7b404f834289786130
MitchBrown532 <55161234+MitchBrown532@users.noreply.github.com>
2024-06-13 15:40:11 -0400

    Fixed error & completed project
    Delete now works as intended. Added a search function, a total cost function that uses derived state from whichever cars are being shown (updates along with the search function. new list = new total). also added some functionality for adding cars which bolds any entries that may be duplicate.

--- cars/src/components/CarList.js
+++ cars/src/components/CarList.js
@@ -2,8 +2,15 @@ import { useDispatch, useSelector } from "react-redux";
 import { removeCar } from "../store";
 function CarList() {
   const dispatch = useDispatch();
-  const cars = useSelector((state) => {
-    return state.cars.data;
+  const { cars, name } = useSelector(({ form, cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return {
+      cars: filteredCars,
+      name: form.name,
+    };
   });
 
   const handleCarDelete = (car) => {
@@ -11,12 +18,17 @@ function CarList() {
   };
 
   const renderedCars = cars.map((car) => {
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
     return (
-      <div key={car.id} className="panel">
+      <div key={car.id} className={`panel ${bold && "bold"}`}>
         <p>
           {car.name} - ${car.cost}
         </p>
-        <button className="button is-danger" onClick={handleCarDelete}>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car)}
+        >
           Delete
         </button>
       </div>
