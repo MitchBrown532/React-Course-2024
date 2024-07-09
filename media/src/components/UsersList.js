9a03523e848ebaf35c087531e3ab6579b4fdd88c
MitchBrown532 <55161234+MitchBrown532@users.noreply.github.com>
2024-07-08 21:54:12 -0400

    adding Albums
    Adding a new components to show albums for each user

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import { useThunk } from "../hooks/use-thunk";
+import UsersListItem from "./UsersListItem";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
 
@@ -29,13 +30,7 @@ function UsersList() {
     content = <div>Error</div>;
   } else {
     content = data.map((user) => {
-      return (
-        <div key={user.id} className="'mb-2 border rounded">
-          <div className="flex p-2 justify-between items-center cursor-pointer">
-            {user.name}
-          </div>
-        </div>
-      );
+      return <UsersListItem key={user.id} user={user} />;
     });
   }
 
